16836151afbb49d09c981ab9b8dfe5b590745c5e
Wididou <fw_dekkiche@esi.dz>
2020-04-21 12:24:13 +0100

    init repository

---
+++ Hospital.py
@@ -0,0 +1,41 @@
+from karel.stanfordkarel import *
+
+
+# File: hospital.py
+# -----------------------------
+# Here is a place to program your section problem
+def main():
+    move()
+    build_hospital()
+    move()
+    move()
+    move()
+    move()
+    build_hospital()
+    move()
+    move()
+    build_hospital()
+
+
+def build_hospital():
+    move()
+    turn_left()
+    move()
+    put_beeper()
+    move()
+    put_beeper()
+    turn_right()
+    move()
+    put_beeper()
+    turn_right()
+    move()
+    put_beeper()
+    move()
+    put_beeper()
+    turn_left()
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()

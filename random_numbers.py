b9d1624db954617e2e3d35025a67c8be5cfda9de
Wididou <fw_dekkiche@esi.dz>
2020-04-26 19:17:39 +0100

    random numbers program

--- random_numbers.py
+++ random_numbers.py
@@ -7,7 +7,9 @@ range from MIN_RANDOM to MAX_RANDOM, inclusive
 
 import random
 
-
+NUM_RANDOM = 10
+MIN_RANDOM = 0
+MAX_RANDOM = 100
 def main():
     """
     You should write your code for this program in this function.
@@ -15,7 +17,8 @@ def main():
     your own code. You should also delete this comment and replace
     it with a better, more descriptive one.
     """
-    pass
+    for i in range(NUM_RANDOM):
+        print(str(random.randint(MIN_RANDOM,MAX_RANDOM)))
 
 
 # This provided line is required at the end of a Python file
